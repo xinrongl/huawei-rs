1e7eeb43531a86ef07295c1d7a0dfbcc2492ea1d
xinrongl <xinronglin@outlook.com>
2020-11-19 16:45:59 +1100

    update inference

--- submission/customize_service.py
+++ submission/customize_service.py
@@ -15,6 +15,7 @@ from model_service.pytorch_model_service import PTServingBaseService
 from PIL import Image
 from torch import Tensor
 from torch.autograd import Variable
+from skimage.morphology import remove_small_holes, remove_small_objects
 
 Image.MAX_IMAGE_PIXELS = 1000000000000000
 logger = log.getLogger(__name__)
@@ -66,6 +67,32 @@ def torch_none(x: Tensor) -> Tensor:
     return x
 
 
+def tta_inference(model, image):
+    output, *_ = model(image)
+
+    for aug, deaug in zip(
+        [torch_rot90, torch_rot180, torch_rot270],
+        [torch_rot270, torch_rot180, torch_rot90],
+    ):
+        tmp, *_ = model(aug(image))
+        x = deaug(tmp)
+        output += torch.sigmoid(x)
+
+    image = torch_transpose(image)
+
+    for aug, deaug in zip(
+        [torch_none, torch_rot90, torch_rot180, torch_rot270],
+        [torch_none, torch_rot270, torch_rot180, torch_rot90],
+    ):
+        tmp, *_ = model(aug(image))
+        x = deaug(tmp)
+        output += torch.sigmoid(torch_transpose(x))
+
+    one_over_8 = float(1.0 / 8.0)
+    out_l_tta = output * one_over_8
+    return out_l_tta
+
+
 class ImageClassificationService(PTServingBaseService):
     def __init__(self, model_name, model_path):
         self.model_name = model_name
@@ -93,11 +120,12 @@ class ImageClassificationService(PTServingBaseService):
             print("Using CPU for inference")
             checkpoint = torch.load(self.model_path, map_location="cpu")
 
-        state_dict = OrderedDict()
-        for key, value in checkpoint["state_dict"].items():
-            tmp = key[7:]
-            state_dict[tmp] = value
-        self.model.load_state_dict(state_dict)
+        # state_dict = OrderedDict()
+        # for key, value in checkpoint["state_dict"].items():
+        #     tmp = key[7:]
+        #     state_dict[tmp] = value
+        # self.model.load_state_dict(state_dict)
+        self.model.load_state_dict(checkpoint["state_dict"])
 
         self.model.eval()
 
@@ -106,88 +134,40 @@ class ImageClassificationService(PTServingBaseService):
         for k, v in data.items():
             for file_name, file_content in v.items():
                 img = Image.open(file_content)
-                # img = self.transforms(img)
                 img = np.array(img)
                 preprocessed_data[k] = img
         return preprocessed_data
 
     def _inference(self, data):
-        image = data["input_img"]
-        data = image
-        ori_x, ori_y = image.shape[0], image.shape[1]
+        img = data["input_img"]
+        data = img
         target_l = 1024
-        stride = 1024
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        if data.max() > 1:
-            data = data / 255.0
 
+        if data.max() > 1:
+            data = data / 255
         data = data - np.array([0.485, 0.456, 0.406])
         data = data / np.array([0.229, 0.224, 0.225])
-        # data = data - np.array([0.444, 0.425, 0.415])
-        # data = data / np.array([0.228, 0.221, 0.231])
-
-        h, w = image.shape[0], image.shape[1]
-        new_w, new_h = w, h
-        if (w - target_l) % stride:
-            new_w = ((w - target_l) // stride + 1) * stride + target_l
-        if (h - target_l) % stride:
-            new_h = ((h - target_l) // stride + 1) * stride + target_l
-        data = cv.copyMakeBorder(
-            data, 0, new_h - h, 0, new_w - w, cv.BORDER_CONSTANT, 0
-        )
         data = data.transpose(2, 0, 1)
 
         c, x, y = data.shape
         label = np.zeros((x, y))
         x_num = (x // target_l + 1) if x % target_l else x // target_l
         y_num = (y // target_l + 1) if y % target_l else y // target_l
-
         for i in range(x_num):
             for j in range(y_num):
                 x_s, x_e = i * target_l, (i + 1) * target_l
                 y_s, y_e = j * target_l, (j + 1) * target_l
-                x_e = min(x_e, x)
-                y_e = min(y_e, y)
                 img = data[:, x_s:x_e, y_s:y_e]
                 img = img[np.newaxis, :, :, :].astype(np.float32)
                 img = torch.from_numpy(img)
                 img = Variable(img.to(device))
 
-                # out_l, *_ = self.model(img)
-                # out_h, *_ = self.model(torch.flip(img, [-1]))
-                # out_h = torch.flip(out_h, [-1])
-                # out_l = F.sigmoid(out_l).cpu().data.numpy()
-                # out_h = F.sigmoid(out_h).cpu().data.numpy()
-                # out_l = (out_l + out_h) / 2.0
-                # out_l = (out_l[0, 1, :, :] > 0.7).astype(np.int8)
-                # out_l = np.argmax(out_l, axis=1)[0]
-                output, *_ = self.model(image)
-
-                for aug, deaug in zip(
-                    [torch_rot90, torch_rot180, torch_rot270],
-                    [torch_rot270, torch_rot180, torch_rot90],
-                ):
-                    tmp, *_ = self.model(aug(image))
-                    x = deaug(tmp)
-                    output += F.sigmoid(x)
-
-                image = torch_transpose(image)
-
-                for aug, deaug in zip(
-                    [torch_none, torch_rot90, torch_rot180, torch_rot270],
-                    [torch_none, torch_rot270, torch_rot180, torch_rot90],
-                ):
-                    tmp, *_ = self.model(aug(image))
-                    x = deaug(tmp)
-                    output += F.sigmoid(torch_transpose(x))
-
-                one_over_8 = float(1.0 / 8.0)
-                out_l = output * one_over_8
+                out_l = tta_inference(self.model, img)
                 out_l = out_l.cpu().data.numpy()
                 out_l = (out_l[0, 1, :, :] > 0.75).astype(np.int8)
+                out_l = np.argmax(out_l, axis=1)[0]
                 label[x_s:x_e, y_s:y_e] = out_l.astype(np.int8)
-
-        label = label[:ori_x, :ori_y]
         # _label = label.astype(np.int8).tolist()
         _label = label.astype(np.int8).tolist()
         _len, __len = len(_label), len(_label[0])
